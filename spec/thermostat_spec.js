cdfb818032320e0bc80797081f585921938fbf38
Tom Lee <tomalee2002@gmail.com>
2021-09-28 16:22:18 +0100

    adds spec test for up()

--- spec/thermostat_spec.js
+++ spec/thermostat_spec.js
@@ -1,6 +1,12 @@
 describe("Thermostat", function () {
+  beforeEach(function () {
+    thermostat = new Thermostat();
+  });
   it("Starts at 20", function () {
-    let thermostat = new Thermostat();
     expect(thermostat.getTemp()).toBe(20);
+  });
+  it("Temperature can be increased", function () {
+    thermostat.up(10);
+    expect(thermostat.getTemp()).toBe(30);
   });
 });
