09cf47327bf56401ab26bfeceb4026c2a3fae9ba
Tom Lee <tomalee2002@gmail.com>
2021-09-29 17:37:46 +0100

    adds power saving mode and assoicated max temp

--- src/thermostat.js
+++ src/thermostat.js
@@ -2,18 +2,33 @@ class Thermostat {
   constructor() {
     this.temperature = 20;
     this.MINIMUM = 10;
+    this.maximum = 25;
+    this.powerSaving = true;
   }
+
   getTemp() {
     return this.temperature;
   }
+
   up(change) {
     this.temperature += change;
   }
+
   down(change) {
     if (this.temperature - change >= this.MINIMUM) {
       this.temperature -= change;
     } else {
       throw "Can't lower temperature as would be below the minimum";
+    }
+  }
+
+  togglePS() {
+    if (this.powerSaving === true) {
+      this.powerSaving = false;
+      this.maximum = 32;
+    } else {
+      this.powerSaving = true;
+      this.maximum = 25;
     }
   }
 }
