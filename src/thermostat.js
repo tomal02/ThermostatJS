1f593175e308e1b24321bd74248b147b09bfd511
Tom Lee <tomalee2002@gmail.com>
2021-09-29 18:18:04 +0100

    adds energyUsage()

--- src/thermostat.js
+++ src/thermostat.js
@@ -35,4 +35,14 @@ class Thermostat {
   reset() {
     this.temperature = 20;
   }
+
+  energyUsage() {
+    if (this.temperature < 18) {
+      return "low-usage";
+    } else if (this.temperature <= 25) {
+      return "medium-usage";
+    } else {
+      return "high-usage";
+    }
+  }
 }
