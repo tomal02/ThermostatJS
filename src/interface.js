04ece44fe4803dcb33ab1f810c07f21c7f6053dc
Sophie Bonneval <maldorana@gmail.com>
2021-09-30 16:36:35 +0100

    API working

--- src/interface.js
+++ src/interface.js
@@ -1,6 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
+
+  fetch('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric')
+  .then((response) => {
+    return response.json()
+  })
+  .then((data) => {
+    document.querySelector('#current-temperature').innerText = data.main.temp;
+  });
+
   const updatedTemp = () => {
     document.querySelector("#temperature").innerText = thermostat.temperature;
+    document.querySelector("#temperature").className = thermostat.energyUsage();
   };
 
   const thermostat = new Thermostat();
@@ -18,9 +28,7 @@ document.addEventListener("DOMContentLoaded", () => {
     thermostat.reset();
     updatedTemp();
   });
-  document
-    .querySelector("#power-saving-toggle")
-    .addEventListener("click", () => {
+  document.querySelector("#power-saving-toggle").addEventListener("click", () => {
       thermostat.togglePS();
       if (thermostat.isPowerSavingOn()) {
         document.querySelector("#power-saving-status").innerText = "ON";
