5c7941fff08313b091a9b4c106eb5c257d6c3a12
Sophie Bonneval <maldorana@gmail.com>
2021-09-30 16:51:08 +0100

    Add API city sorting

--- src/interface.js
+++ src/interface.js
@@ -1,11 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-  fetch('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric')
-  .then((response) => {
-    return response.json()
-  })
-  .then((data) => {
-    document.querySelector('#current-temperature').innerText = data.main.temp;
+  const selectElement = document.querySelector('#current-city');
+  selectElement.addEventListener('change', (event) => {
+    const city = event.target.value;
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric`
+    
+    fetch(url)
+      .then((response) => {
+        return response.json()
+      })
+      .then((data) => {
+        document.querySelector('#current-temperature').innerText = data.main.temp;
+      })
   });
 
   const updatedTemp = () => {
